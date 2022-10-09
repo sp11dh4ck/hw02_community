d5c434aeb1e3e8db9e00ea4db46e4d793db56e9f
Zakhar <94287281+sp11dh4ck@users.noreply.github.com>
2022-10-09 14:12:00 +0300

    #up

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Post, Group
 
-# Register your models here.
+
 class PostAdmin(admin.ModelAdmin):
     list_display = (
         'pk',
--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -17,7 +17,7 @@ class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(
-        User, 
+        User,
         on_delete=models.CASCADE,
         related_name='posts'
     )
