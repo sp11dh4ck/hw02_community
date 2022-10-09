cb7adc6871d4d1d613c8d3972ef1b5dbe54f926a
Zakhar <94287281+sp11dh4ck@users.noreply.github.com>
2022-10-09 13:22:21 +0300

    #up

---
+++ yatube/posts/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class Group(models.Model):
+    title = models.CharField(max_length=30)
+    slug = models.SlugField(unique=True, verbose_name='Уникальный адрес')
+    descriptions = models.TextField()
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Post(models.Model):
+    text = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        User, 
+        on_delete=models.CASCADE,
+        related_name='posts'
+    )
+    group = models.ForeignKey(
+        Group,
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE
+    )
