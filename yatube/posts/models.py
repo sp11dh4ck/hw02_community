7a1115b08229284b7bcdcdcc676bd3190e1c8c2c
Zakhar <94287281+sp11dh4ck@users.noreply.github.com>
2022-10-11 17:25:41 +0300

    #up
    2-е обновление под задание ревьюера, остался последний рывок))

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -5,25 +5,35 @@ User = get_user_model()
 
 
 class Group(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200, verbose_name='Заглавие')
     slug = models.SlugField(unique=True, verbose_name='Уникальный адрес')
-    description = models.TextField()
+    description = models.TextField(verbose_name='Описание')
 
     def __str__(self) -> str:
         return self.title
 
+    class Meta:
+        verbose_name_plural = 'Группы'
+        ordering = ['-title']
+
 
 class Post(models.Model):
-    text = models.TextField()
-    pub_date = models.DateTimeField(auto_now_add=True)
+    text = models.TextField(verbose_name='Текст')
+    pub_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата')
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='posts'
+        related_name='posts',
+        verbose_name='Автор'
     )
     group = models.ForeignKey(
         Group,
         blank=True,
         null=True,
-        on_delete=models.CASCADE
+        on_delete=models.SET_NULL,
+        verbose_name='Группа'
     )
+
+    class Meta:
+        verbose_name_plural = 'Посты'
+        ordering = ['-pub_date']
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -6,7 +6,7 @@ from .models import Group, Post
 def index(request):
     template = 'posts/index.html'
     number_posts = 10
-    posts = Post.objects.order_by('-pub_date')[:number_posts]
+    posts = Post.objects.all()[:number_posts]
     context = {
         'posts': posts,
     }
@@ -17,7 +17,7 @@ def group_posts(request, slug):
     template = 'posts/group_list.html'
     group = get_object_or_404(Group, slug=slug)
     number_posts = 10
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:number_posts]
+    posts = Post.objects.filter(group=group).all()[:number_posts]
     context = {
         'group': group,
         'posts': posts,
