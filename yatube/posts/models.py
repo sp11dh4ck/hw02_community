322bc2ae5e1b73002da8a9c85758d986a5154e75
Zakhar <94287281+sp11dh4ck@users.noreply.github.com>
2022-10-13 00:20:50 +0300

    #up

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -5,26 +5,35 @@ User = get_user_model()
 
 
 class Group(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200, verbose_name='Название')
     slug = models.SlugField(unique=True, verbose_name='Уникальный адрес')
-    description = models.TextField()
+    description = models.TextField(verbose_name='Описание')
 
     def __str__(self) -> str:
         return self.title
 
+    class Meta:
+        verbose_name_plural = 'Группы'
+        ordering = ['-title']
+
 
 class Post(models.Model):
-    text = models.TextField()
-    pub_date = models.DateTimeField(auto_now_add=True)
+    text = models.TextField(verbose_name='Текст')
+    pub_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата')
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
+        verbose_name='Автор',
         related_name='posts'
     )
     group = models.ForeignKey(
         Group,
+        verbose_name='Группа',
         on_delete=models.SET_NULL,
         blank=True,
-        null=True,
-        on_delete=models.CASCADE
+        null=True
     )
+
+    class Meta:
+        verbose_name_plural = 'Посты'
+        ordering = ['-pub_date']
