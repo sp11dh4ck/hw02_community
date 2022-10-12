765ce0c35a3283fbb1ffc6c12da0caeb04c265ac
Zakhar <94287281+sp11dh4ck@users.noreply.github.com>
2022-10-13 00:11:19 +0300

    #up
    Я всё сделаль :)

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,29 +1,40 @@
-from django.db import models
 from django.contrib.auth import get_user_model
+from django.db import models
 
 User = get_user_model()
 
 
 class Group(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200, verbose_name='Заглавие')
     slug = models.SlugField(unique=True, verbose_name='Уникальный адрес')
-    description = models.TextField()
+    description = models.TextField(verbose_name='Описание')
 
     def __str__(self) -> str:
         return self.title
 
+    class Meta:
+        verbose_name_plural = 'Группы'
+        ordering = ['-title']
+
 
 class Post(models.Model):
-    text = models.TextField()
-    pub_date = models.DateTimeField(auto_now_add=True)
+    text = models.TextField(verbose_name='Текст')
+    pub_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата')
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='posts'
+        related_name='posts',
+        verbose_name='Автор'
     )
     group = models.ForeignKey(
         Group,
+        on_delete=models.SET_NULL,
         blank=True,
         null=True,
-        on_delete=models.CASCADE
+        verbose_name='Группа',
+        related_name='posts'
     )
+
+    class Meta:
+        verbose_name_plural = 'Посты'
+        ordering = ['-pub_date']
