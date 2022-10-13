8c1eed03fff61d312eab811cb5abf53f810db647
Zakhar <94287281+sp11dh4ck@users.noreply.github.com>
2022-10-13 18:33:39 +0300

    #up
    PEP8

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -16,8 +16,12 @@ def index(request):
 def group_posts(request, slug):
     template = 'posts/group_list.html'
     number_posts = 10
-    # я не понял, как реализовать prefetch_related и избавиться от 'posts': posts
-    group = get_object_or_404(Group.objects.all().prefetch_related(), slug=slug)
+    """
+    я не понял, как реализовать 
+    prefetch_related и избавиться от 'posts': posts
+    """
+    group = get_object_or_404(Group.objects.all().prefetch_related(),
+                              slug=slug)
     posts = Post.objects.select_related('group').all()[:number_posts]
     context = {
         'group': group,
