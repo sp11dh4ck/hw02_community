df1738c679275058730e58eab9682560e47b7056
Zakhar <94287281+sp11dh4ck@users.noreply.github.com>
2022-10-13 18:48:00 +0300

    #up

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -17,7 +17,7 @@ def group_posts(request, slug):
     template = 'posts/group_list.html'
     number_posts = 10
     """
-    я не понял, как реализовать 
+    я не понял, как реализовать
     prefetch_related и избавиться от 'posts': posts
     """
     group = get_object_or_404(Group.objects.all().prefetch_related(),
